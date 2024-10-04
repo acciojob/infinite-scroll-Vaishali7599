65ecd089a4905f9f485926185d5efa4a853d9ce0
Vaishali7599 <140495282+Vaishali7599@users.noreply.github.com>
2024-10-04 23:08:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here!
+const ol = document.querySelector("ol");
+
+for(let i = 1; i<=10; i++){
+	const li = document.createElement("li");
+	li.innerText = `Item ${i}`
+	ol.append(li)
+}
+
+let curr = 10;
+function addTwoMore(curr){
+	for(let i = 1; i<=2; i++){
+		const li = document.createElement("li");
+		li.innerText = `Item ${curr + 1}`;
+		ol.append(li);
+		curr++;
+	}
+}
+
+ol.addEventListener('scroll', function(){
+	if(window.innerHeight + window.scrollY >= document.body.offsetHeight){
+		addTwoMore(curr)
+		curr = curr+2;
+	}
+})
 
